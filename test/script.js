1ef8070d5013ae6da26668cec13c7bc932e029fb
Parisa Fallahi <134124565+parisa-fallahi@users.noreply.github.com>
2026-05-12 17:32:25 +0330

    javascript document updated

--- test/script.js
+++ test/script.js
@@ -1,20 +1,61 @@
+let customers = [
+  {
+    id: 1,
+    f_name: "Abby",
+    l_name: "Thomas",
+    gender: "M",
+    married: true,
+    age: 32,
+    expense: 500,
+    purchased: ["Shampoo", "Toys", "Book"],
+  },
+  {
+    id: 2,
+    f_name: "Jerry",
+    l_name: "Tom",
+    gender: "M",
+    married: true,
+    age: 64,
+    expense: 100,
+    purchased: ["Stick", "Blade"],
+  },
+  {
+    id: 3,
+    f_name: "Dianna",
+    l_name: "Cherry",
+    gender: "F",
+    married: true,
+    age: 22,
+    expense: 1500,
+    purchased: ["Lipstik", "Nail Polish", "Bag", "Book"],
+  },
+  {
+    id: 4,
+    f_name: "Dev",
+    l_name: "Currian",
+    gender: "M",
+    married: true,
+    age: 82,
+    expense: 90,
+    purchased: ["Book"],
+  },
+  {
+    id: 5,
+    f_name: "Maria",
+    l_name: "Gomes",
+    gender: "F",
+    married: false,
+    age: 7,
+    expense: 300,
+    purchased: ["Toys"],
+  },
+];
 
-const res = [
-    {key:'test',value:'weyutywe'},
-    {key:'test1',value:'test-1-1'},
-    {key:'test2',value:'test-2-2'},
-    {key:'test3',value:'weyutywe'},
-    {key:'test4',value:'weyutywe'},
-    {key:'test5',value:'weyutywe'},
-]
 
+let _result = customers.map((customer)=>{
+    const _title = customer.gender === "M" ? 'Mr.' : (customer.married ? 'Mrs.' : 'Miss.')
+    customer.fullName = ` ${_title} ${customer.f_name} ${customer.l_name}`
 
-    const _test = res.map((i)=>{
-      const _value = i.value;
-      const findText = _value.slice(0,8)
-      if(findText === "weyutywe"){
-        return findText
-      }
-    }) 
+})
 
-    console.log(_test)
+console.log(customers);
